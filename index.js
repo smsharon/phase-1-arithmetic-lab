1a46eed3578fcf75389046757b0413d6329288c6
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-12 07:47:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log (multiply);
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console. log (random);
+
+let num3= 20;
+let num4 = 4;
+let mod = (num3 % num4) + 4;
+
+let numbers = [10, 5, 20, 8, 15];
+let max = Math.max(...numbers);
+
+console.log (max);
+
+
